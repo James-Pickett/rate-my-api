97534b5b2380fb82202f1c9bd30c4ce796759313
James Pickett <jamesandrewpickett@gmail.com>
2018-01-19 19:59:54 -0800

    removed mongodb driver and added mongoose set up lint

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ console.log('woo');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
-const ObjectID = mongodb.ObjectID;
 
 const APIS_COLLECTION = 'contacts';
 
-var app = express();
+const app = express();
 app.use(bodyParser.json());
 
-app.listen(3000, function(){
-    console.log('listenting on 300');
-})
+app.listen(3000, () => {
+  console.log('listenting on 300');
+});
