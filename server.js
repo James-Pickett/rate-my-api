351701baa099772afe8e9e316ae054f7cead1308
James Pickett <jamesandrewpickett@gmail.com>
2018-01-19 21:14:33 -0800

    no able to connect directly to db from box moved db credentials to local file and deleted old user from db since I comitted user name and password to public repo, but really the odds are like 1/9999999999999 that some would care, but I suppose it's good practice

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const mongoose = require('mongoose');
+const express = require('express'); // ExperssJS Framework
+const mongoose = require('mongoose'); // mongoose ORM
+const localConfig = require('./local-config.json');
 
-const mongoDb = 'mongodb://dev:waBaby1!@ds263317.mlab.com:63317/heroku_n61mbsk0';
+const mongoDb = localConfig.dbUrl;
 
-mongoose.connect(mongoDb, {
-  useMongoClient: true,
-});
+mongoose.connect(mongoDb);
+
+const app = express();
+const port = process.env.port || 8080;
 
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.listen(port, () => {
+  console.log(`Running the server on port ${port}`);
+});
