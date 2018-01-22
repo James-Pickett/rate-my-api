bf7218955cc76be6fb322f659d106ffa6f6ce49a
James Pickett <jamesandrewpickett@gmail.com>
2018-01-21 20:49:23 -0800

    cleaned up server.js file remove lint object destructing rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     "extends": "airbnb",
     "rules": {
         "linebreak-style": ["error", "windows"],
-        "no-underscore-dangle": ["error", { "allowAfterThis": true }]
+        "no-underscore-dangle": ["error", { "allowAfterThis": true }],
+        "prefer-destructuring": 0
     }
 };
--- server.js
+++ server.js
@@ -3,26 +3,25 @@ const mongoose = require('mongoose'); // mongoose ORM
 const fs = require('fs'); // file stream
 const indexRoute = require('./server/routes/index.route'); // api index routes file
 
+const app = express();
+app.use('/api/v1.0', indexRoute); // default route to read index routes
+
 let dbUrl = '';
 
+// use local config file if it exists
 if (fs.existsSync('./local-config.json')) {
   const rawData = fs.readFileSync('./local-config.json');
   dbUrl = JSON.parse(rawData).dbUrl;
+  mongoose.connect(dbUrl);
   console.log('Using local-config.json');
 } else {
   dbUrl = process.env.MONGODB_URI;
 }
 
 mongoose.connect(dbUrl);
-
-const app = express();
-
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-app.use('/api/v1.0', indexRoute);
 
 const server = app.listen(process.env.PORT || 8080, () => {
   const port = server.address().port;
