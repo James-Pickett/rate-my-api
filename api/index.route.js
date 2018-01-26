5309b94bddfb20cf9371d370c9e60b46855bc88d
James Pickett <jamesandrewpickett@gmail.com>
2018-01-25 21:00:48 -0800

    please work arg . . .

--- api/index.route.js
+++ api/index.route.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const apiRoute = require('./school/school.route');
+const schoolsRoute = require('./school/school.route');
 
 const router = express.Router();
 
-router.use('/schools', apiRoute);
+router.use('/schools', schoolsRoute);
 
 module.exports = router;
