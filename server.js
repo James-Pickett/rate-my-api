02588ab09d4f52fdfe105fc8a70d77732a51f33f
James Pickett <jamesandrewpickett@gmail.com>
2018-02-03 14:25:00 -0800

    testing travis ci unit test

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose'); // mongoose ORM
 const bodyParser = require('body-parser'); // json body-parser
 const fs = require('fs'); // file stream
 const indexRoute = require('./api/index.route'); // api index routes file
-const morgan = require('morgan'); // some logging thing?
 const config = require('config');
+const argv = require('minimist')(process.argv.slice(2));
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,23 +13,24 @@ app.use(bodyParser.json({ type: 'application/json' }));
 app.use(bodyParser.text());
 app.use('/api/v1', indexRoute); // default route to read index routes
 
-let dbUrl = '';
+let dbHost = '';
 
 if (fs.existsSync('./config')) { // check to see if we have a config folder
+  console.log('Using local config dbHost');
   let rawData = fs.readFileSync('./config/dev.json'); // load up the dev json
   if (config.util.getEnv('NODE_ENV') === 'test') { // the we are in the test enviornment
     rawData = fs.readFileSync('./config/test.json'); // load up the test json
-  } else {
-    app.use(morgan('combined')); // 'combined' outputs the Apache style LOGS
-    console.log('Using morgan \'combined\' logs');
   }
-  dbUrl = JSON.parse(rawData).DBHost;
-  console.log('Using local config');
+  dbHost = JSON.parse(rawData).DBHost;
+} else if (argv.dbHost) {
+  console.log('Using command line arg dbHost');
+  dbHost = argv.dbHost;
 } else {
-  dbUrl = process.env.MONGODB_URI;
+  console.log('Using process.env.MONGODB_URI dbHost');
+  dbHost = process.env.MONGODB_URI;
 }
 
-mongoose.connect(dbUrl);
+mongoose.connect(dbHost);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
